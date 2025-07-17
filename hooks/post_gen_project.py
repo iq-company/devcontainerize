561ad5b2acc1a8d968a5c42e22015d8cc94d2027
Philipp Gruener <philipp.gruener@royal-software.de>
2025-07-17 16:09:02 +0200

    relocated commands to tmp dir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,32 @@
-# import os import shutil
-# 
-# target = ".devcontainer"
-# template = ".devcontainer_tpl"
-# 
-# if os.path.exists(target):
-#     response = input(f"{target} existiert bereits. Überschreiben? [y/N] ").strip().lower()
-#     if response != "y":
-#         print("Abbruch.")
-#         exit(1)
-#     shutil.rmtree(target)
-# 
-# shutil.move(template, target)
-# print(f"{target} wurde erfolgreich eingerichtet.")
-# 
+import os
+import subprocess
+import sys
+
+def run_bash(cmd):
+    print(f"Running: {cmd}")
+    subprocess.run(cmd, shell=True, check=True)
+
+def fail(message):
+    print(f"[ERROR] {message}")
+    sys.exit(1)
+
+if __name__ == "__main__":
+    # `cookiecutter` starts in the output dir of the generated project
+
+    # devcontainer_path = os.path.join(project_root, ".devcontainer")
+    # if os.path.exists(devcontainer_path):
+    #     fail(".devcontainer already setup. Skipping")
+
+    # current_dir = os.path.abspath(os.path.curdir)
+    project_root = os.getcwd()
+    tmp_app_dir = os.path.join(project_root, "_tmp_app_name_source")
+    target_app_dir = os.path.join(project_root, "{{cookiecutter.app_name}}")
+    run_bash(f"mv {os.path.join(tmp_app_dir, '*.py')} {target_app_dir}")
+    run_bash(f"mv {os.path.join(tmp_app_dir, 'commands/*')} {os.path.join(target_app_dir, 'commands')}")
+
+
+    # dynamisch home_dir setzen, wenn nicht manuell überschrieben
+    username = "{{ cookiecutter.username }}"
+    home_dir = "/home/{{ cookiecutter.username }}"
+    os.environ["HOME_DIR"] = home_dir
+
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,18 +1,6 @@
 import os
-import sys
-
-def fail(message):
-    print(f"[ERROR] {message}")
-    sys.exit(1)
-
-project_root = os.getcwd()
-devcontainer_path = os.path.join(project_root, ".devcontainer")
-
-if os.path.exists(devcontainer_path):
-    fail(".devcontainer already setup. Skipping")
 
 # dynamisch home_dir setzen, wenn nicht manuell überschrieben
 username = "{{ cookiecutter.username }}"
 home_dir = "/home/{{ cookiecutter.username }}"
 os.environ["HOME_DIR"] = home_dir
-
